aa10ac84f60a3773ceebae01bedf525961c46f44
unknown <holupkern@gmail.com>
2024-03-11 15:32:30 +0800

    completed qn 5 w ans

---
+++ Round-22-Animal-Adoption/index.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", async function(){
+
+    const body = document.querySelector("body");
+
+    const response = await axios.get("./data.json");
+    // be mindful of the data type. No response.data here.
+
+    const responseData = response.data;
+
+    for (let item of response.data){
+        
+        const listObject = document.createElement("ul");
+        listObject.innerHTML = `
+            <li> Name: ${item.Name} </li>
+            <li> Type of Animal ${item["Type of Animal"]} </li>
+            <li> Breed: ${item.Breed} </li>
+            <li> Date of Birth: ${item["Date of Birth"]} </li>
+            <li> Medical Condition: ${item["Medical Conditions"]} </li>
+        `
+        const listImage = document.createElement("img");
+        listImage.setAttribute("src", `${item.Photo}`)
+
+        body.appendChild(listObject);
+        body.appendChild(listImage);
+    }
+})
